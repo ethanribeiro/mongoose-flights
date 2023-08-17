262c46519c385f5ff4da09fc6e1f50ad5bc18ad0
ethanribeiro <ethanribeiro101@gmail.com>
2023-08-17 17:08:52 -0400

    Finalized mongoose-flights part 1. Modified: index.ejs flights, style.css stylesheet with special late-departure class, and flights.js controller

--- controllers/flights.js
+++ controllers/flights.js
@@ -25,7 +25,7 @@ function newFlight(req, res) {
     const fnMax = Flight.schema.path('flightNo').options.max;
     // Obtain the default date
     const dt = newFlight.departs;
-    let departsDate = `${(dt.getFullYear() + 1)}-${(dt.getMonth() + 1).toString().padStart(2, '0')}-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+    let departsDate = `${(dt.getFullYear())}-${(dt.getMonth() + 1).toString().padStart(2, '0')}-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
     res.render('flights/new', { departsDate, fnMin, fnMax, title: "Add Flight", errorMsg: "" });
 }
 
